7e03f73310298c387e8715e8372d87d3221f3e6d
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-10-03 21:46:12 +0300

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-
-
+for dig1 in range(0, 10):
+    for dig2 in range(dig1 + 1, 10):
+        if dig1 == 8 and dig2 == 9:
+            print("{}{}".format(dig1, dig2))
+        else:
+            print("{}{}".format(dig1, dig2), end=", ")
