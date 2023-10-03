da05210715f1c38321fa6701d6df96b7ce60de8b
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-10-03 21:43:41 +0300

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -4,4 +4,4 @@ for number in range(0, 100):
     if number == 99:
         print("{}".format(number))
     else:
-        printO("{:02}".format(number), end=", ")
+        print("{:02}".format(number), end=", ")
