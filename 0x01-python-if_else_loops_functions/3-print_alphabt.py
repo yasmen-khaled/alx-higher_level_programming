8d3230349cd441ae6307ae33c28782fa61272284
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-10-03 20:22:07 +0300

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for alphabet in range(97, 123):
-    if chr(alphabet) is not 'q' and chr(alphabet) is not 'e':
-        print("{}".format(chr(alphabet)), end="")
+for alpha in range(97, 123):
+    if chr(alpha) is not 'q' and chr(alpha) is not 'e':
+        print("{}".format(chr(alpha)), end="")
