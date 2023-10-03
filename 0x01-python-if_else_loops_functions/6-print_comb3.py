bc29a27399589a2c7089ba4b8dfe649c5e968005
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-10-03 22:14:08 +0300

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 for digit1 in range(0, 10):
     for digit2 in range(digit1 + 1, 10):
-        if digit1 == 9 and digit2 == 8:
-            print("{}{}".format(digit1, digit2))
-        else:
-            print("{}{}".format(digit1, digit2), end=", ")
+        print("{}{}".format(digit1, digit2), end=", ")
