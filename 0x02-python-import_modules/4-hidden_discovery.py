0ab1f0d4c1397db278d3e3ffeb9375a270034e8b
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-10-06 01:39:20 +0300

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-
     import hidden_4
-
-    names = dir(hidden_4)
-    for name in names:
+    n = dir(hidden_4)
+    for name in n:
         if name[:2] != "__":
             print(name)
