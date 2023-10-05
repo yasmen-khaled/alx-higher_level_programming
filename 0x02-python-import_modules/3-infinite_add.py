467d9be66eabc2e316a3e3de70894de96cf558c7
yasmen <yasmen.khaled2000@gmail.com>
2023-10-06 01:12:54 +0300

    up

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
+import sys
+
 if __name__ == "__main__":
 
-    import sys
-    numbers = 0
-    for i in range(len(sys.argv) - 1):
-        numbers += int(sys.argv[i + 1])
-        print("{}".format(numbers))
+    num = 0
+    for i in range(1, len(sys.argv)):
+        num += int(sys.argv[i])
+        print("{}".format(num))
