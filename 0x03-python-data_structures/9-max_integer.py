c205aed939181c034a30ecd645a976314b8910c4
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-10-09 16:34:03 +0300

    Update 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,8 +3,8 @@
 def max_integer(my_list=[]):
     if len(my_list)  == 0:
         return (None)
-    bigest = my_list[0]
+    max = my_list[0]
     for x in range(len(my_list)):
-        if my_list[x] > bigest:
-            bigest = my_list[x]
-            return (bigest)
+        if my_list[x] > max:
+            max = my_list[x]
+            return (max)
