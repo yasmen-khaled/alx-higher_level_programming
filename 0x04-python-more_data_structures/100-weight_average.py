3fcf4f1d85b5fe96942c418a1736c5eb6b17378f
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-10-11 22:41:23 +0300

    Update 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,12 +2,12 @@
 
 def weight_average(my_list=[]):
 
-        if not _list:
-                    return 0
-                    number = 0
-                        av = 0
-                            for i in _list:
-                                        number += i[0] * i[1]
-                                                av += i[1]
+    if not _list:
+        return 0
+    number = 0
+    av = 0
+    for i in _list:
+        number += i[0] * i[1]
+        av += i[1]
 
-                                                    return (number / av)
+    return (number / av)
