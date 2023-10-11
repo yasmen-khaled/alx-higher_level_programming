e06a86ab2162222d331594cd0412bffc6e3a2c16
yasmen <yasmen.khaled2000@gmail.com>
2023-10-11 22:40:58 +0300

    done

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+
+        if not _list:
+                    return 0
+                    number = 0
+                        av = 0
+                            for i in _list:
+                                        number += i[0] * i[1]
+                                                av += i[1]
+
+                                                    return (number / av)
