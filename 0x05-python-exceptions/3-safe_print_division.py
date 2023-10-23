fcd044fec21a9db3f33ed09a3ede39389953379a
yasmen <yasmen.khaled2000@gmail.com>
2023-10-23 23:05:34 +0300

    done

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        divid = a / b
+    except (TypeError, ZeroDivisionError):
+        divid = None
+    finally:
+        print("Inside result: {}".format(divid))
+            return (divid)
