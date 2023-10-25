b9003fe4cc4f5277cc1c75b2e46430fe0b7d4a97
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-10-25 03:53:19 +0300

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""class Square."""
+
 
 class Square:
+    """square."""
+
     def __init__(self, size=0):
+        """Initialize a new Square"""
+
+        
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
