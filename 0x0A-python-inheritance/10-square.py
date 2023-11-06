caf76d171a2a3ac9598c28853b49a65e6c0d7fad
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-11-07 01:06:06 +0300

    done

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Representing"""
+
+    def __init__(self, size):
+        """Initialize square"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
