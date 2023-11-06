f7f273acd614eb6772de139e0fbea980ff079e58
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-11-06 21:49:44 +0300

    done

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define class"""
+
+def is_kind_of_class(obj, a_class):
+    """Check if an object"""
+
+    if isinstance(obj, a_class):
+        return True
+    return False
