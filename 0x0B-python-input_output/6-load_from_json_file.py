4e695c1cf7c5ef85184ba8b0afcd9e53b2a49241
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-11-08 00:42:58 +0300

    Done

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -3,7 +3,7 @@
 import json
 
 
-def save_to_json_file(my_obj, filename):
-    """Write object"""
-    with open(filename, "w") as f:
-        json.dump(my_obj, f)
+def load_from_json_file(filename):
+    """Createing"""
+    with open(filename) as f:
+        return json.load(f)
