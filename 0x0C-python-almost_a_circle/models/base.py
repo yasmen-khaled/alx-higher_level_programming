8a20ecba5e722f48544061201480c55ddca2f0f0
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-11-14 02:26:05 +0300

    Ok enough!

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,13 +5,14 @@ import json
 import csv
 import turtle
 
+
 class Base:
-    """Base class for other classes."""
+    """Base"""
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize a new instance of the Base class."""
+        """Initializein"""
         if id is not None:
             self.id = id
         else:
@@ -20,14 +21,14 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Convert a list of dictionaries to a JSON string."""
+        """Return"""
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """Save a list of objects to a JSON file."""
+        """Write json"""
         filename = cls.__name__ + ".json"
         with open(filename, "w") as jsonfile:
             if list_objs is None:
@@ -38,14 +39,14 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
-        """Convert a JSON string to a list of dictionaries."""
+        """Return_"""
         if json_string is None or json_string == "[]":
             return []
         return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
-        """Create a new instance of the class from a dictionary."""
+        """Return again"""
         if dictionary and dictionary != {}:
             if cls.__name__ == "Rectangle":
                 new = cls(1, 1)
@@ -56,7 +57,7 @@ class Base:
 
     @classmethod
     def load_from_file(cls):
-        """Load a list of objects from a JSON file."""
+        """Return anthor one"""
         filename = str(cls.__name__) + ".json"
         try:
             with open(filename, "r") as jsonfile:
@@ -65,9 +66,9 @@ class Base:
         except IOError:
             return []
 
-@classmethod
+    @classmethod
     def save_to_file_csv(cls, list_objs):
-        """CSV """
+        """ CSV"""
         filename = cls.__name__ + ".csv"
         with open(filename, "w", newline="") as csvfile:
             if list_objs is None or list_objs == []:
@@ -100,7 +101,7 @@ class Base:
 
     @staticmethod
     def draw(list_rectangles, list_squares):
-        """Draw Rectangles"""
+        """Drawing a Rectangles"""
         turt = turtle.Turtle()
         turt.screen.bgcolor("#b7312c")
         turt.pensize(3)
