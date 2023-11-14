2174187dfbcedc66c3c555c520398ea086bcc3cb
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-11-14 04:51:20 +0300

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 
+"""Defines a base model class."""
 import json
-import csv
-import turtle
-from random import random
-import time
-from os.path import exists
 
 
 class Base:
@@ -67,62 +63,3 @@ class Base:
                 return [cls.create(**d) for d in list_dicts]
         except IOError:
             return []
-
-    @classmethod
-    def save_to_file_csv(cls, list_objs):
-        """Save a list of objects to a CSV file."""
-        name = cls.__name__
-        ls = []
-        if list_objs is not None:
-            if name == "Rectangle":
-                for a in list_objs:
-                    ls.append([a.id, a.width, a.height, a.x, a.y])
-            else:
-                for a in list_objs:
-                    ls.append([a.id, a.size, a.x, a.y])
-        with open(name + ".csv", "w+", encoding="utf-8", newline='') as file:
-            writer = csv.writer(file)
-            writer.writerows(ls)
-
-    @classmethod
-    def load_from_file_csv(cls):
-        """Load a list of objects from a CSV file."""
-        name = cls.__name__
-        if not exists(name + ".csv"):
-            return []
-        with open(name + ".csv", "r+", encoding="utf-8", newline='') as file:
-            tmp = csv.reader(file)
-            ls = []
-            for a in tmp:
-                a = [int(i) for i in a]
-                if name == "Rectangle":
-                    dic = {"id": a[0], "width": a[1], "height": a[2],
-                           "x": a[3], "y": a[4]}
-                elif name == "Square":
-                    dic = {"id": a[0], "size": a[1], "x": a[2], "y": a[3]}
-                else:
-                    return []
-                ls.append(cls.create(**dic))
-        return ls
-
-    @staticmethod
-    def draw(list_rectangles, list_squares):
-        """Draw rectangles and squares using the turtle module."""
-        shapes = list_rectangles + list_squares
-        for a in shapes:
-            draw = turtle.Turtle()
-            draw.color(random(), random(), random())
-            draw.setpos(-a.x, -a.y)
-            draw.pensize(7)
-            draw.pendown()
-            draw.forward(a.width)
-            draw.right(90)
-            draw.forward(a.height)
-            draw.right(90)
-            draw.forward(a.width)
-            draw.right(90)
-            draw.forward(a.height)
-            draw.right(90)
-            draw.end_fill()
-
-        time.sleep(10)
