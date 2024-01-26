9688d9a67cf235db6dafccb913e6792fa39d16a5
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2024-01-27 01:42:36 +0300

    Done

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Takes"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+    print(response.headers.get('X-Request-Id'))
