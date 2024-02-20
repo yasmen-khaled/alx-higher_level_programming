e2bc38f82f4c2a64f30e5f389853f76250ec02cb
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2024-02-21 00:51:41 +0300

    done

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/*
+script that writes a string to a file.
+*/
+const fs = require('fs');
+
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+    return (-1);
+  }
+});
