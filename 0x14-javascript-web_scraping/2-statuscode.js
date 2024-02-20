5a1b64ba8d90801e3d91609cf15dc7f15b3f5850
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2024-02-21 00:52:51 +0300

    done

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/*
+script that display the status code of a GET request
+*/
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
